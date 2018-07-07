cdc067af4ef2cfe0008c09b88b4a707ab584b398
Cogithub2016 <judas.liscariote.007@gmail.com>
2018-07-07 16:37:36 +0200

    Add GET /todos route

--- playground/others.js
+++ playground/others.js
@@ -2,6 +2,7 @@ var person = function Person(first) {
   this.firstName = first;
  }
 var user = {name: 'andrew', age: 25, timeid : function() { return Date();}}
+var usebix= [thingbis= {name:'andrew'},25, function() { return Date();}];
 var {name} = user;
 console.log(name);
 var {age , timeid} = user;
@@ -11,3 +12,12 @@ var obj=new timeid();
 console.log('obj',obj);
 var {thing} = user;
 console.log(thing);
+
+/*var {thingbis} = usebix;
+ 
+ console.log('thb', thingbis);*/
+/* what does "var {something}" do? 
+you're talking about destructuring.
+var something creates a variable called something and stores whatever you assign to it.
+ var { something } when assigned an object will look for a property called something on that object and assign its value to the variable something.
+ Mark as helpful (3) */
--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.post('/todos', (req, res) => {
     res.send(doc);}, (e) =>{
       res.status(400).send(e);});
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    console.log(todos);
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+module.exports = {app};
