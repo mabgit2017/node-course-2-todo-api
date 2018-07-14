dac01539d7fe2ae94f6df718806441a50cb23aa3
Cogithub2016 <judas.liscariote.007@gmail.com>
2018-07-14 20:16:18 +0200

    Set up app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var app = express();
+const port =  process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -27,7 +28,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  var id= req.params.id;
+  var id = req.params.id;
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
@@ -41,10 +42,24 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(! ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+ }).catch((e) => {
+  res.status(400).send();
+ });
+});
 
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 module.exports = {app};
