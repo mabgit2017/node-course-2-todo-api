3cb514f5ce5c2ed97956b99a7b6a7c4c49beeb89
Cogithub2016 <judas.liscariote.007@gmail.com>
2018-07-15 17:43:51 +0200

    Add Patch todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 
-
+const _=require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -57,7 +57,27 @@ app.delete('/todos/:id', (req, res) => {
  });
 });
 
-
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(! ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set : body} , {new : true}).then((todo) =>{
+      if(!todo){
+        return res.status(404).send();
+      }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
